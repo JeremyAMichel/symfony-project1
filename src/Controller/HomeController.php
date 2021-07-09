c19983195fc354a7f93ece65a7a9d7e54112333d
June <rem-je42@hotmail.fr>
2021-07-09 13:27:21 +0200

    ajout HomeController + registration + auth + modif user

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,7 +12,9 @@ class HomeController extends AbstractController
      * @Route("/home", name="home")
      */
     public function index(): Response
-    {
+    {   
+        $user = $this->getUser();
+        dump($user);
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
